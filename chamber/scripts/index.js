8fd503fb023560890d2cad23314dfd0b6f93689c
nessKenya <nelson.muchonji@gmail.com>
2023-01-29 00:39:39 +0300

    Mombasa Chambers of Commerce Homepage

---
+++ chamber/scripts/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#year").innerHTML = new Date().getFullYear();
+  document.getElementById("updated").innerHTML = new Date();
+  let today = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(new Date());
+  document.querySelector("#today").innerHTML = today;
+});
+
+function openMenu(){
+  document.querySelector(".page-links").classList.remove("hidden");
+  document.querySelector("#open-btn").classList.add("hidden");
+  document.querySelector("#close-btn").classList.remove("hidden");
+}
+
+function closeMenu() {
+  document.querySelector(".page-links").classList.add("hidden");
+  document.querySelector("#open-btn").classList.remove("hidden");
+  document.querySelector("#close-btn").classList.add("hidden");
+}
